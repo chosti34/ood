25f5be858ce50fef2f5d329670055add7c5cdd8c
Timur <chosti34@gmail.com>
2018-03-15 14:35:27 +0300

    Добавлены тесты для входного потока - файла

--- Decorator/Streams/StreamsTests/FileInputStreamTests.cpp
+++ Decorator/Streams/StreamsTests/FileInputStreamTests.cpp
@@ -1,4 +1,37 @@
 #include "stdafx.h"
 #include "../FileInputStream.h"
 
+BOOST_AUTO_TEST_SUITE(FileInputStreamTests)
+	BOOST_AUTO_TEST_CASE(stream_throws_exception_when_not_existing_file_path_is_specified)
+	{
+		BOOST_REQUIRE_THROW(FileInputStream("not-existing-file.txt"), std::runtime_error);
+		{
+			std::ofstream file("existing-file.txt");
+			BOOST_REQUIRE_NO_THROW(FileInputStream("existing-file.txt"));
+		}
+		int removed = std::remove("existing-file.txt");
+		BOOST_CHECK_EQUAL(removed, 0);
+	}
 
+	BOOST_AUTO_TEST_CASE(stream_reached_eof_automatically_when_empty_file_is_opened)
+	{
+		{
+			std::ofstream file("empty.txt");
+			FileInputStream strm("empty.txt");
+			BOOST_CHECK(strm.IsEOF());
+			BOOST_REQUIRE_THROW(strm.ReadByte(), std::ios_base::failure);
+		}
+		int removed = std::remove("empty.txt");
+		BOOST_CHECK_EQUAL(removed, 0);
+	}
+
+	BOOST_AUTO_TEST_CASE(stream_can_correcly_read_whole_file_content_per_one_byte)
+	{
+		// TODO: implement this
+	}
+
+	BOOST_AUTO_TEST_CASE(stream_can_correctly_read_whole_content_per_byte_blocks)
+	{
+		// TODO: implement this
+	}
+BOOST_AUTO_TEST_SUITE_END()
---
+++ Decorator/Streams/StreamsTests/FileOutputStreamTests.cpp
@@ -0,0 +1,5 @@
+#include "stdafx.h"
+#include "../FileOutputStream.h"
+
+BOOST_AUTO_TEST_SUITE(FileOutputStreamTests)
+BOOST_AUTO_TEST_SUITE_END()
